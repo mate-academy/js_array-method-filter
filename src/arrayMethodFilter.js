b80fcbb39b7e024fd8f551443c7cf696e0b4570c
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 14:11:19 +0300

    implemented task and tests

---
+++ src/arrayMethodFilter.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Splice
+ */
+function applyCustomFilter() {
+  [].__proto__.filter2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomFilter;
