d4d46f0668ef56f6b1a0142ae79e8c7faa0c5e52
Olegus <gulldan1996@gmail.com>
2019-04-10 16:28:55 +0300

    arrayMethodFilter

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,8 +4,18 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
-  [].__proto__.filter2 = function() {
-    // write code here
+  [].__proto__.filter2 = function(callback) {
+    const items = this;
+    const resultItems = [];
+
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+
+      if (callback(item, i, items)) {
+        resultItems.push(item);
+      }
+    }
+    return resultItems;
   };
 }
 
