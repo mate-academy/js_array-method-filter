615e6fabc055fc13a9be7a88c813c9c3818b022d
OleksandrHlybchenko <logista@meta.ua>
2019-06-18 22:26:16 +0300

    for review

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const changedArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this) === true) {
+        changedArray[changedArray.length] = this[i];
+      }
+    }
+    return changedArray;
   };
 }
 
