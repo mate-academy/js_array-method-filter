fadbeb8130b65125b368d3e3ef6f63136f741dbe
Alc499 <Ali.vitchenko@gmail.com>
2019-06-20 16:49:49 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filteredArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+
+      if (callback(item, i, this) === true) {
+        filteredArray[filteredArray.length] = item;
+      }
+    }
+
+    return filteredArray;
   };
 }
 
