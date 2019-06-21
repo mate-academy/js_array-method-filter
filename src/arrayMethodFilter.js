29f15db3916a67bd352b3a9103221dab8ff6aa6a
Пользователь <Sviatoslv>
2019-06-21 15:40:17 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,8 +5,38 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+      const index = i;
+      const arr = this;
+
+      if (callback(item, index, arr)) {
+        result[result.length] = item;
+      }
+    }
+
+    return result;
   };
 }
 
 module.exports = applyCustomFilter;
+
+// [].__proto__.filter2 = function(callback) {
+//   const result = [];
+
+//   for (let i = 0; i < this.length; i++) {
+//     const item = this[i];
+//     const index = i;
+//     const arr = this;
+
+//     if(callback(item, index, arr)) {
+//       result[result.length] = item;
+//     }
+//   }
+
+//   return result;
+// };
+
+// console.log([0, 10, 20, 30].filter2(item => item > 10));
