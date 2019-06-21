aa84dd901de977d2774476a2931a9619decdbf37
Пользователь <Sviatoslv>
2019-06-21 15:42:22 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -22,21 +22,3 @@ function applyCustomFilter() {
 }
 
 module.exports = applyCustomFilter;
-
-// [].__proto__.filter2 = function(callback) {
-//   const result = [];
-
-//   for (let i = 0; i < this.length; i++) {
-//     const item = this[i];
-//     const index = i;
-//     const arr = this;
-
-//     if(callback(item, index, arr)) {
-//       result[result.length] = item;
-//     }
-//   }
-
-//   return result;
-// };
-
-// console.log([0, 10, 20, 30].filter2(item => item > 10));
