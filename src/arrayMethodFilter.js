f2472dddced9cd6e753ef726f70b0e9d2a466e1f
h2ash <hun22ash@gmail.com>
2019-06-21 11:48:14 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,6 +6,13 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     // write code here
+    const finalArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        finalArray[finalArray.length] = this[i];
+      }
+    }
+    return finalArray;
   };
 }
 
