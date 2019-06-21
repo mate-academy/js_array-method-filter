4059da8409328b8cfcd4553c19de51a81fd21979
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-21 09:20:52 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,14 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const resultArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        resultArray[resultArray.length] = this[i];
+      }
+    }
+
+    return resultArray;
   };
 }
 
