13cc6ce519f184d8a4ca520a7903712508b5b593
RavenVol <r.volianiuk@gmail.com>
2019-06-22 00:21:35 +0300

    Filter2 - Solved by Raven

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,6 +6,17 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     // write code here
+    let counter = 0;
+    const filteredArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && callback(this[i], i, this)) {
+        filteredArray[counter] = this[i];
+        counter++;
+      }
+    }
+
+    return filteredArray;
   };
 }
 
