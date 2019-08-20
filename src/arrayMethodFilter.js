0a1c49071c4772d548761c5dc513783e74ca7c67
Volodsher <volodsher@gmail.com>
2019-08-21 00:05:46 +0300

    this one was very easy

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        newArray.push(this[i]);
+      }
+    }
+    return newArray;
   };
 }
 
