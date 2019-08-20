17499b137b5822454567becf382c051b475ebf85
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-08-20 14:58:45 +0300

    deleted extra variable elementsCount

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,12 +6,10 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const filteredArr = [];
-    let elementsCount = 0;
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filteredArr[elementsCount] = this[i];
-        elementsCount++;
+        filteredArr[filteredArr.length] = this[i];
       }
     }
 
