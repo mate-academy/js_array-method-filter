8755ccc9e3b13420eaa473f3bc4f75d05352a05c
aleks-mos <mossssss@ukr.net>
2019-08-20 18:08:05 +0300

    create doc

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,6 +6,14 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     // write code here
+    const arrFilter = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        arrFilter.push(this[i]);
+      }
+    }
+    return arrFilter;
   };
 }
 
