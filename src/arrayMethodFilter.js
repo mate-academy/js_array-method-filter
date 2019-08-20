7aaa8818366d1e645f8d6d76ae59d65fd0d336a8
KseniiaMazan <ksusamazan@gmail.com>
2019-08-20 18:53:42 +0300

    removed .push() method

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,11 +6,15 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const resultArr = [];
+    let counter = 0;
+
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        resultArr.push(this[i]);
+        resultArr[counter] = this[i];
+        counter++;
       }
     }
+
     return resultArr;
   };
 }
