4386379e1539225cf001604bbc26b5ce55ba4eec
youvovk <wer3wollf@gmail.com>
2019-08-21 18:25:47 +0300

    delete unnecessary variable

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,12 +6,10 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const filteredArr = [];
-    let countIndex = 0;
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filteredArr[countIndex] = this[i];
-        countIndex++;
+        filteredArr[filteredArr.length] = this[i];
       }
     }
 
