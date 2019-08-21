54073730c40b22a4ba4e168fccf47767ae55fff2
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-21 18:21:42 +0300

    refactoring

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,12 +6,10 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const newArray = [];
-    let index = 0;
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        newArray[index] = this[i];
-        index++;
+        newArray[newArray.length] = this[i];
       }
     }
     return newArray;
