20cb0bb0896e34a0fd38b54f5a39ead8b677980c
Rafler <amuronyk@gmail.com>
2019-08-21 12:18:55 +0300

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,14 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filterArr = [];
+    for (let i = 0; i < this.length; i++) {
+      const result = callback(this[i], i, this);
+      if (result) {
+        filterArr[filterArr.length] = this[i];
+      }
+    }
+    return filterArr;
   };
 }
 
