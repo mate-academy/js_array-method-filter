be1dddc97c6cae5cc36477d6a8042b3767a37b3d
Taisiia <taisiia.pomazan@gmail.com>
2019-08-21 18:16:06 +0300

    sol

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,11 +6,10 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const filtered = [];
-    let index = 0;
+
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filtered[index] = this[i];
-        index++;
+        filtered[filtered.length] = this[i];
       }
     }
     return filtered;
