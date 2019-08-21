4da21b35e33e181a5c8f7bcc905ec4f76386d111
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-21 18:33:45 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,15 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const arrayAfterFilter = [];
+
+    for (let i = 0; i < this.length; i += 1) {
+      if (callback(this[i], i, this)) {
+        arrayAfterFilter.push(this[i]);
+      }
+    }
+
+    return arrayAfterFilter;
   };
 }
 
