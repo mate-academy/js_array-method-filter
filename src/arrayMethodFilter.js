50208d9d4fb1309edfbdbab0add9e94ab1a5c5aa
aleks-mos <mossssss@ukr.net>
2019-08-21 11:46:57 +0300

    change doc

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,14 +6,14 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     // write code here
-    const arrFilter = [];
+    const filteredArray = [];
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        arrFilter.push(this[i]);
+        filteredArray[filteredArray.length] = this[i];
       }
     }
-    return arrFilter;
+    return filteredArray;
   };
 }
 
