e5621c64ea806cc35829798ce84f50859759ea20
youvovk <wer3wollf@gmail.com>
2019-08-21 13:33:51 +0300

    change push

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,10 +6,12 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const filterArr = [];
+    let countIndex = 0;
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filterArr.push(this[i]);
+        filterArr[countIndex] = this[i];
+        countIndex++;
       }
     }
 
