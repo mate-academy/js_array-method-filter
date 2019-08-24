ba0547c16646572315e91db43717e4999a3fa6ff
vladyslav223 <vladyslav223@gmail.com>
2019-08-24 04:27:34 +0300

    tipa done

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,8 +4,14 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
+  const filtered = [];
   [].__proto__.filter2 = function(callback) {
     // write code here
+    for (let i = 0; i < this.length; i++) {
+      filtered[i] = callback(this[i], i, this);
+    }
+
+    return filtered;
   };
 }
 
