10341ef70517dfe29b7cac3c767992d429154c81
Shurkodr <sashaglazunov2@gmail.com>
2019-10-02 18:56:15 +0300

    passed local tests

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,14 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const FilterArr = [];
+    let count = 0;
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this) === true) {
+        FilterArr[count++] = this[i];
+      }
+    }
+    return FilterArr;
   };
 }
 
