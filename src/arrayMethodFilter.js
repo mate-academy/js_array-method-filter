6e9b9dada24b8f30c731ff2bba45e4c1f34b3e97
Sijey <popov040391@gmail.com>
2019-10-02 12:23:21 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -7,12 +7,8 @@ function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const newArray = [];
 
-    this.forEach((item, index, arr) => {
-      if (callback(item, index, arr)) {
-        newArray.push(item);
-      }
-      return item;
-    });
+    this.forEach((item, index, arr) => callback(item, index, arr)
+      ? newArray.push(item) : item);
 
     return newArray;
   };
