5447e1c7775837d5da2891ca3b9cf8ed30409a11
Leon <FeelineKing@gmail.com>
2019-10-03 15:23:36 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -3,8 +3,4 @@
 /**
  * Implement method Filter
  */
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};
-while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}
-('4 d(){8.a.7=4(5){9 3=[];f(b 1=0;1<2.6;1++){c(5(2[1],1,2)){3[3.6]=2[1]}}e 3}}',16,16,'|i|this|filterResults|function|callback|length|filter2|Array|const|prototype|let|if|applyCustomFilter|return|for'.split('|'),0,{}))
-
-module.exports = applyCustomFilter;
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('6 5(){b.a.9=6(7){c 2=[];d(g 1=0;1<3.4;1++){e(7(3[1],1,3)){2[2.4]=3[1]}}f 2}}h.8=5;',18,18,'|i|filterResults|this|length|applyCustomFilter|function|callback|exports|filter2|prototype|Array|const|for|if|return|let|module'.split('|'),0,{}))
