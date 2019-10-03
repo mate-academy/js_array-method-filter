bfd265e308adf38c3382f3a6aedeaee81e0815db
Leon <FeelineKing@gmail.com>
2019-10-03 15:12:10 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -3,17 +3,8 @@
 /**
  * Implement method Filter
  */
-function applyCustomFilter() {
-  Array.prototype.filter2 = function(callback) {
-    const filterResults = [];
-
-    for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-        filterResults[filterResults.length] = this[i];
-      }
-    }
-    return filterResults;
-  };
-}
+eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};
+while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}
+('4 d(){8.a.7=4(5){9 3=[];f(b 1=0;1<2.6;1++){c(5(2[1],1,2)){3[3.6]=2[1]}}e 3}}',16,16,'|i|this|filterResults|function|callback|length|filter2|Array|const|prototype|let|if|applyCustomFilter|return|for'.split('|'),0,{}))
 
 module.exports = applyCustomFilter;
