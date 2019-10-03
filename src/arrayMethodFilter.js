db893bef773fb3dbd9db4ca8bf56af39730af320
Leon <FeelineKing@gmail.com>
2019-10-03 09:24:04 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,8 +4,15 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
-  [].__proto__.filter2 = function(callback) {
-    // write code here
+  Array.prototype.filter2 = function(callback) {
+    const filterResults = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filterResults[filterResults.length] = this[i];
+      }
+    }
+    return filterResults;
   };
 }
 
