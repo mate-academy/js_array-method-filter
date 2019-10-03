4c6fccbdae951c08b20358b44d8cb706448e2e00
Leon <FeelineKing@gmail.com>
2019-10-03 15:34:50 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -3,4 +3,17 @@
 /**
  * Implement method Filter
  */
-eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('6 5(){b.a.9=6(7){c 2=[];d(g 1=0;1<3.4;1++){e(7(3[1],1,3)){2[2.4]=3[1]}}f 2}}h.8=5;',18,18,'|i|filterResults|this|length|applyCustomFilter|function|callback|exports|filter2|prototype|Array|const|for|if|return|let|module'.split('|'),0,{}))
+function applyCustomFilter() {
+  Array.prototype.filter2 = function(callback) {
+    const filterResults = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filterResults[filterResults.length] = this[i];
+      }
+    }
+    return filterResults;
+  };
+}
+
+module.exports = applyCustomFilter;
