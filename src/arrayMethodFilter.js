1a7e1cf235d3f89621de5b78c5f4a8ed39caa783
Oles Marola <oles_pw@ukr.net>
2019-11-19 20:42:25 +0200

    filter

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,16 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filtered = [];
+    if (this.length === 0) {
+      return this;
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filtered[filtered.length] = this[i];
+      }
+    }
+    return filtered;
   };
 }
 
