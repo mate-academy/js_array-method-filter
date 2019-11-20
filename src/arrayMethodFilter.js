f90324c2549248e8765824e0c7d5fbd3795ab048
cs-cognitome <gaya.scienza.v@gmail.com>
2019-11-20 18:17:25 +0200

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,18 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const result = [];
+
+    if (this.length === 0) {
+      return this;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i]);
+      }
+    }
+    return result;
   };
 }
 
