91969bd91b10e66bee99dd83247b0e07206950a7
Vivych Oksana <vivichokcana@gmail.com>
2019-11-20 17:13:43 +0200

    'Solution'

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filterArr = [];
+    for (let index = 0; index < this.length; index++) {
+      if (callback(this[index], index, this) === true) {
+        filterArr[filterArr.length] = this[index];
+      }
+    }
+    return filterArr;
   };
 }
 
