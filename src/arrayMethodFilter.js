f8e8afc1f56cfdd281c6c89159e10f0619e12336
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-20 20:44:28 +0200

    Aidinov Alexii

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,12 +6,10 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     const result = [];
-    let index = 0;
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        result[index] = this[i];
-        index++;
+        result[result.length] = this[i];
       }
     }
 
