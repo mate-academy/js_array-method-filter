c2b264ae2f814a64a127e5a7b30a8cb40c718714
ksukhaldun <kseniya.khaldun@gmail.com>
2019-11-20 19:16:01 +0300

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filterArr = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filterArr.push(this[i]);
+      }
+    }
+    return filterArr;
   };
 }
 
