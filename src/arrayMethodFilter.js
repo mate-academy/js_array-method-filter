8b55d0278f06dc64fd85ed8cbda9c2d565002701
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-20 15:10:01 +0200

    develop

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filt = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filt.push(this[i]);
+      }
+    }
+    return filt;
   };
 }
 
