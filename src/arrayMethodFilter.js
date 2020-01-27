01da135c57d6ef651de30eebfcffc299b151b25f
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-27 16:39:52 +0200

    done

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,19 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    if (!this.length) {
+      return [];
+    }
+
+    const filtered = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filtered[filtered.length] = this[i];
+      }
+    }
+
+    return filtered;
   };
 }
 
