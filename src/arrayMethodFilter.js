a26f88139e9dc401a9c718fa943ca61e599dca23
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-27 16:44:34 +0200

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,15 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i]);
+      };
+    }
+
+    return result;
   };
 }
 
