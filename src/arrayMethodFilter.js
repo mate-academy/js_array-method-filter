b0b947162996e84ac3c1690cb8cca8f4d088a600
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-28 16:04:54 +0200

    with custom push

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,12 +4,20 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
+  [].__proto__.push2 = function(...elements) {
+    for (let i = 0; i < elements.length; i++) {
+      this[this.length] = elements[i];
+    }
+
+    return this.length;
+  };
+
   [].__proto__.filter2 = function(callback) {
     const filtered = [];
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filtered.push(this[i]);
+        filtered.push2(this[i]);
       }
     }
 
