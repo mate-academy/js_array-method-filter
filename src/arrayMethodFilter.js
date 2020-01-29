eafe8859ddaccb0538e522ca88d58c0fc0fa146d
Leonid <matembit1996@gmail.com>
2020-01-29 15:45:31 +0200

    task was done

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,18 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filterItems = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+      const isTestPassed = callback(item, i, this);
+
+      if (isTestPassed) {
+        filterItems[filterItems.length] = item;
+      }
+    }
+
+    return filterItems;
   };
 }
 
