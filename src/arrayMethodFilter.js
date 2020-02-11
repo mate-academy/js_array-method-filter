e87d92e0e5c89a62a03e7b1d94fbf6d119bd8e48
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-11 12:30:00 +0200

    first

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -3,9 +3,23 @@
 /**
  * Implement method Filter
  */
+// function length(item) {
+//   return item.length >= 6;
+// }
+// let newArr = [];
+// let words = ['roma', 'natalya', 'polya'];
+
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const newArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        newArr.push(this[i]);
+      }
+    }
+
+    return newArr;
   };
 }
 
