141a93232c1c708a70f717d6a40691335c39a4d3
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-03 17:49:23 +0300

    Solution lint_fix

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,16 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const items = this;
+    const arr = [];
+
+    for (let i = 0; i < items.length; i++) {
+      if (callback(items[i], i, items)) {
+        arr.push(items[i]);
+      }
+    }
+
+    return arr;
   };
 }
 
