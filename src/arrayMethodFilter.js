7f1ec2e64f66c2cf5168a881d9c4318f852dc52f
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-04-04 17:31:11 +0300

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,8 +4,15 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
-  [].__proto__.filter2 = function(callback) {
-    // write code here
+  [].__proto__.filter2 = function (callback) {
+    const resultArr = [];
+    
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        resultArr.push(this[i]);
+      }
+    }
+    return resultArr;
   };
 }
 
