d4f3c0ea3a10d0fb4c65138d1c61376d50e5e77b
Igor Kulish <igormagnetic@gmail.com>
2020-04-05 17:25:16 +0300

    sol

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -4,8 +4,16 @@
  * Implement method Filter
  */
 function applyCustomFilter() {
-  [].__proto__.filter2 = function(callback) {
-    // write code here
+  [].__proto__.filter2 = function (callback) {
+    let result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i])
+      }
+    }
+
+    return result
   };
 }
 
