b214f5b0d793f098dee5c12af3ca3510ecba8f81
Vitalii Shytikov <v.shytikov@gmail.com>
2020-04-05 16:00:12 +0300

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filtered = [];
+    let filteredIndex = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filtered[filteredIndex] = this[i];
+        filteredIndex++;
+      }
+    }
+
+    return filtered;
   };
 }
 
