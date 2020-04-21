4f3198d59f81390463b27eb084c00fc26bab30f5
Vitaliy <vitaliyanohin@gmail.com>
2020-04-21 11:28:43 +0300

    sol

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -6,6 +6,15 @@
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
     // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this) === true) {
+        result.push(this[i]);
+      }
+    }
+
+    return result;
   };
 }
 
