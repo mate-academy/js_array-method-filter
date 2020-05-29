ddc36b047c56a32c426e7711c9381670b065b12e
Vovasef <vovasef@gmail.com>
2020-05-29 16:41:36 +0300

    Solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,8 +5,18 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i]);
+      }
+    }
+
+    return result;
   };
 }
 
+// applyCustomFilter();
+// console.log([0, 10, 20, 30].filter2(x => x > 10));
 module.exports = applyCustomFilter;
