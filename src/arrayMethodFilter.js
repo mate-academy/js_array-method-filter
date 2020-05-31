d5f877c81c940649ba59692042115bff49715919
DTR <t.terry.rice@gmail.com>
2020-05-31 13:55:02 +0300

    final 1.1

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,13 +5,11 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    let o = 0;
     const filtered = [];
 
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        filtered[o] = this[i];
-        o++;
+        filtered[filtered.length] = this[i];
       }
     }
 
