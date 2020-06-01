ca7a7e728b4a7425425a5e2741331e6cfb66584e
Slava Polishchuk <dizerllc@gmail.com>
2020-06-01 14:00:41 +0300

    solution

--- src/arrayMethodFilter.js
+++ src/arrayMethodFilter.js
@@ -5,7 +5,15 @@
  */
 function applyCustomFilter() {
   [].__proto__.filter2 = function(callback) {
-    // write code here
+    const filteredValues = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        filteredValues.push(this[i]);
+      }
+    }
+
+    return filteredValues;
   };
 }
 
